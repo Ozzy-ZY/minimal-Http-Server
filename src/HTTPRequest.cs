74bf8a4261f769389e93d26058dea0ee3e2eaad9
Ozzy-ZY <Zyadhimself1@gmail.com>
2025-01-15 21:37:27 +0200

    new answers and http class

---
+++ src/HTTPRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace codecrafters_http_server;
+
+
+public class HTTPRequest()
+{
+    public string Method { get; set; }
+    public string Path { get; set; }
+    public string Version { get; set; }
+}
--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using codecrafters_http_server;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -9,4 +10,16 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 var socket = server.AcceptSocket(); // wait for client
-socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+var responsebuffer = new byte[1024];
+var requestData = socket.Receive(responsebuffer);
+var linesOfResponse = Encoding.ASCII.GetString(responsebuffer).Split("\r\n"); // \r\n is the CRLF
+var line0 = linesOfResponse[0].Split(" ");
+var request = new HTTPRequest()
+{
+    Method = line0[0],
+    Path = line0[1],
+    Version = line0[2]
+};
+var response = request.Path == "/"? $"{request.Version} 200 OK\r\n\r\n"
+                                    :$"{request.Version} 404 Not Found\r\n\r\n";
+socket.Send(Encoding.UTF8.GetBytes(response));
