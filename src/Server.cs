de47fb2430442b88222d2348529201b70cbe68dd
Ozzy-ZY <Zyadhimself1@gmail.com>
2025-01-17 00:27:39 +0200

    support for concurrent connections and refactoring and error handling

--- src/Server.cs
+++ src/Server.cs
@@ -1,43 +1,82 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using codecrafters_http_server;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
+namespace codecrafters_http_server;
 
-// Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
-server.Start();
-var socket = server.AcceptSocket(); // wait for client
-var responsebuffer = new byte[1024];
-var requestData = socket.Receive(responsebuffer);
-var linesOfResponse = Encoding.ASCII.GetString(responsebuffer).Split("\r\n"); // \r\n is the CRLF
-var line0 = linesOfResponse[0].Split(" ");
-var request = new HTTPRequest()
+internal class Program
 {
-    Method = line0[0],
-    Path = line0[1],
-    Version = line0[2]
-};
-Console.WriteLine($"Got the request {request}");
-string response;
-if (request.Path == "/")
-    response = $"{request.Version} 200 OK\r\n\r\n";
-else if (request.Path.StartsWith("/echo/"))
-{
-    string msg = request.Path[6..];
-    response = $"{request.Version} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {msg.Length}\r\n\r\n{msg}";
+    private static void Main()
+    {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        Console.WriteLine("Logs from your program will appear here!");
+
+        TcpListener server = new TcpListener(IPAddress.Any, 4221);
+        server.Start();
+        while (true)
+        {
+            var socket = server.AcceptSocket(); // wait for client
+            Task.Run(() => HandleRequest(socket));
+            
+        }
+    }
+
+    static Task HandleRequest(Socket socket)
+    {
+        try
+        {
+            Console.WriteLine($"Received request from {socket.RemoteEndPoint}");
+            var responsebuffer = new byte[1024];
+            socket.Receive(responsebuffer);
+            var linesOfResponse = Encoding.ASCII.GetString(responsebuffer).Split("\r\n"); // \r\n is the CRLF
+            var line0 = linesOfResponse[0].Split(" ");
+            if (line0.Length < 3)
+            {
+                Console.WriteLine("Invalid request line");
+                socket.Send("HTTP/1.1 400 Bad Request\r\n\r\n"u8.ToArray());
+                return Task.CompletedTask;
+            }
+
+            var request = new HTTPRequest()
+            {
+                Method = line0[0],
+                Path = line0[1],
+                Version = line0[2]
+            };
+            Console.WriteLine($"Got the request {request}");
+            string response;
+            if (request.Path == "/")
+                response = $"{request.Version} 200 OK\r\n\r\n";
+            else if (request.Path.StartsWith("/echo/"))
+            {
+                string msg = request.Path[6..];
+                response =
+                    $"{request.Version} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {msg.Length}\r\n\r\n{msg}";
+            }
+
+            else if (request.Path.StartsWith("/user-agent"))
+            {
+                var header =
+                    linesOfResponse.First(x =>
+                        x.ToLower().Contains("user-agent: ")); // finding the header from the headers
+                var userAgentValue = header.Replace(header[..12], ""); // removing the header key leaving the value
+                response =
+                    $"{request.Version} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {userAgentValue.Length}\r\n\r\n{userAgentValue}";
+            }
+            else
+                response = $"{request.Version} 404 Not Found\r\n\r\n";
+
+            socket.Send(Encoding.UTF8.GetBytes(response));
+            Console.WriteLine($"send the response\n{response}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            socket.Close();
+        }
+        return Task.CompletedTask;
+    }
 }
-
-else if (request.Path.StartsWith("/user-agent"))
-{
-    var header = linesOfResponse.First(x => x.ToLower().Contains("user-agent: ")); // finding the header from the headers
-    var userAgentValue = header.Replace(header[..12], ""); // removing the header key leaving the value
-    response = 
-        $"{request.Version} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {userAgentValue.Length}\r\n\r\n{userAgentValue}";
-}
-else
-    response = $"{request.Version} 404 Not Found\r\n\r\n";
-socket.Send(Encoding.UTF8.GetBytes(response));
-Console.WriteLine($"send the response\n{response}");
